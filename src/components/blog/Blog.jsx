6c5ca679ce50d18d1c2380eabb464baab70c5489
Mike Ta <h.michael.ta@gmail.com>
2020-10-23 16:07:23 +0200

    Add blog component and get posts from database

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-const Blog = () => {
-    return (
-        <div>
+export default class Blog extends Component {
+    constructor(props) {
+        super(props)
 
-        </div>
-    );
+        this.state = {
+            posts: [],
+        }
+    }
+
+    getAllPosts = () => {
+        axios
+            .get(`http://localhost:5000/data`)
+            .then((response) => {
+                console.log(response.data)
+                const posts = response.data
+                this.setState({
+                    posts: posts,
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    componentDidMount() {
+        this.getAllPosts()
+    }
+
+    render() {
+        return (
+            <div className="Blog">
+                {this.state.posts
+                    .map(post => (
+                        <div className="Blog__Post-Container" key={post.id}>
+                            <p className="Blog__Post">{post.timeStamp} {post.dateSymptomsAppeared.split('-').reverse().join('-')}</p>
+                        </div>
+                    ))}
+            </div>
+        )
+    }
 }
-
-export default Blog;
