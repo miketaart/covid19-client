aeef6eb3e06b5e9364bf02ab21efeab876790149
Mike Ta <h.michael.ta@gmail.com>
2020-10-23 10:42:00 +0200

    Add form with hard coded questions to survey component

--- src/components/survey/Survey.jsx
+++ src/components/survey/Survey.jsx
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import axios from 'axios';
 
-const Survey = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+export default class Survey extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            nickName: '',
+            email: '',
+            sex: '',
+            dateSymptomsAppeared: '',
+            q1: '',
+            q2: '',
+            symptoms: [
+                { value: "cough", isChecked: false },
+                { value: "fever", isChecked: false },
+                { value: "headache", isChecked: false },
+                { value: "musclepain", isChecked: false }
+            ],
+            date: '',
+            comment: '',
+            timeStamp: '',
+        }
+    }
+
+    render() {
+        return (
+            <div className="Survey">
+                <div className="Survey_Header-Container">
+                    <p className="Survey_Header">Share your experience</p>
+                </div>
+
+                <form className="Survey__Form">
+                    <label>
+                        <input
+                            placeholder="Add first name"
+                        />
+                    </label>
+                    <label>
+                        <input
+                            placeholder="Date of birth"
+                            type="date"
+                        />
+                    </label>
+                    <div>
+                        <input
+                            type="radio"
+                            value='male'
+                        />
+                        <label htmlFor="sex">male</label>
+                        <input
+                            type="radio"
+                            value='female'
+                        />
+                        <label htmlFor="sex">female</label>
+                    </div>
+                    <label>
+                        <input
+                        type="email"
+                            placeholder="Email"
+                        />
+                    </label>
+                    <label>
+                        <input
+                            placeholder="q1"
+                        />
+                    </label>
+                    <label>
+                        <input
+                            placeholder="q2"
+                        />
+                    </label>
+                    <label>
+                        <input
+                            placeholder="comment"
+                        />
+                    </label>
+
+                    <button className="formButton" >Add experience</button>
+                </form>
+            </div>
+        )
+    }
 }
-
-export default Survey;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar';
 import Dashboard from '../components/dashboard/Dashboard';
-// import Survey from '../components/survey/Survey';
+import Survey from '../components/survey/Survey';
 // import Blog from '../components/blog/Blog';
 // import Footer from '../components/footer/Footer';
 
@@ -10,6 +10,7 @@ const Home = () => {
         <div>
             <Navbar />
             <Dashboard />
+            <Survey />
         </div>
     );
 }
