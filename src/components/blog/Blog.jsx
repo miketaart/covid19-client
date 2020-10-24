bc2708b76b125c84c39706085d6c6b136c853993
Mike Ta <h.michael.ta@gmail.com>
2020-10-24 18:57:58 +0200

    Add sort functionality to list overview component

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -27,6 +27,30 @@ export default class Blog extends Component {
             })
     }
 
+    sortEarliestLogic = (a, b) => {
+        a = a.date.split('-').reverse().join('');
+        b = b.date.split('-').reverse().join('');
+        return a.localeCompare(b);
+    }
+
+    sortLatestLogic = (a, b) => {
+        a = a.date.split('-').reverse().join('');
+        b = b.date.split('-').reverse().join('');
+        return b.localeCompare(a);
+    }
+
+    sortByEarliest = () => {
+        this.setState({
+            posts: this.state.posts.sort(this.sortEarliestLogic),
+        });
+    };
+
+    sortByLatest = () => {
+        this.setState({
+            posts: this.state.posts.sort(this.sortLatestLogic),
+        });
+    };
+
     updateSearch = (e) => {
         this.setState({
             search: e.target.value
@@ -43,6 +67,8 @@ export default class Blog extends Component {
         return (
             <div className="Blog-Container">
                 <input className="input" type="text" placeholder="Search in messages" value={this.state.search} onChange={this.updateSearch} />
+                <button onClick={this.sortByLatest} className="sort-button">Sort by latest</button>
+                <button onClick={this.sortByEarliest} className="sort-button sort-button-active">Sort by earliest</button>
                 <div className="Blog">
                     {this.state.posts
                         .filter((post) =>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Time from './Time';
 import Date from './Date';
-import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
@@ -9,7 +8,8 @@ const Navbar = () => {
             <div className="Navbar__logo-container">
                 <img style={{ width: "50px", outline: "1px solid #F80701" }} className="Navbar__logo" src="https://image.freepik.com/vrije-vector/covid-19-rode-logo-sjabloon_23-2148501246.jpg" alt="logo" />
             </div>
-            <Link to="/posts/survey" className="">Share you experience</Link>
+            <h1>The Covid-19 Experience</h1>
+
             <div style={{ width: "250px", outline: "1px solid #F80701" }} className="Navbar__TimeDate-container">
                 <Date />
                 <Time />
